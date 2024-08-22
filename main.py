36d4dd5f49855356c5f4e058a54481e16930f6b4
eivindkolmannskog <eivind.sk@hotmail.com>
2024-08-22 10:54:20 +0200

    testing from main

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ from overleaf_syncing.SyncClient import SyncClient
 
 def main():
     sync_client = SyncClient()
+    sync_client.remove_all_synced_folders()
+
     sync_client.add_synced_folder("results_1")
-    sync_client.add_synced_folder("results_2")
+    sync_client.add_synced_folder("results_2")  
+    
 
     print(sync_client)
     
     sync_client.sync()
+    sync_client.erase_old_files()
 
 
 if __name__ == "__main__":
