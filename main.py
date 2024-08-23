cef1289857d4cacb182aa1cb0d7d0b6b2940d4eb
eivindkolmannskog <eivind.sk@hotmail.com>
2024-08-23 11:08:34 +0200

    restructure

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from overleaf_syncing.SyncClient import SyncClient
+from src.overleaf_syncing.SyncClient import SyncClient
 
 def main():
-    sync_client = SyncClient()
+    sync_client = SyncClient(reset=True)
     #sync_client.remove_all_synced_folders()
 
-    sync_client.add_synced_folder("results_1")
-    sync_client.add_synced_folder("results_2")  
+    #sync_client.add_synced_folder("results_1")
+    #sync_client.add_synced_folder("results_2")  
     
-    sync_client.upload_synced_folders()
+    #sync_client.upload_synced_folders()
 
 
 
