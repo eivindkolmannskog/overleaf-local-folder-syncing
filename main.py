fcf48c02324daa9d8fda9bc2727bb352d72b9dcf
eivindkolmannskog <eivind.sk@hotmail.com>
2024-08-24 12:41:51 +0200

    changes to main

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-from overleaf_syncing.OverleafSyncing import SyncClient
+import os
+from src.overleaf_syncing.OverleafSyncing import OverleafSyncing
 
 def main():
-    sync_client = SyncClient(dropbox_url="https://www.dropbox.com/home/Apper/Project%20Thesis")
-    #sync_client.remove_all_synced_folders()
+    client = OverleafSyncing(dropbox_url="https://www.dropbox.com/home/Apper/Overleaf/Project%20Thesis")
 
-    #sync_client.add_synced_folder("results_1")
-    #sync_client.add_synced_folder("results_2")  
-    
-    #sync_client.upload_synced_folders()
+    root = os.path.dirname(os.path.abspath(__file__))
+
+    path_1 = os.path.join(root, "mock", "results_1")
+    path_2 = os.path.join(root, "mock", "results_2")
+    client.add_synced_directories(path_1)
+    client.add_synced_directories(path_2)
+
+    client.upload_synced_folders()
 
 
 
